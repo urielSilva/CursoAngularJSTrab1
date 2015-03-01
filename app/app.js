d53cf51bd36795d4cac3dc0ba38b86c2d7abada9
urielSilva <urielsilva@cjr.org.br>
2015-03-01 14:59:55 -0300

    Primeiro commit

---
+++ app/app.js
@@ -0,0 +1,6 @@
+'use strict';
+
+// Declare app level module which depends on views, and components
+var enejApp = angular.module('enejApp', []);
+
+
---
+++ app/profile/profile.js
@@ -0,0 +1,37 @@
+/**
+ * Created by Uriel on 27/02/2015.
+ */
+
+enejApp.controller('profileController', function() {
+    this.usuario =
+    {
+        "nomeUsuario" : "",
+        "email" : "",
+        "senha" : "",
+        "nome" : "",
+        "sobrenome" : "",
+        "nomeCracha" : "",
+        "cpf" : "",
+        "rg" : "",
+        "sexo" : "",
+        "cep" : "",
+        "pais" : "",
+        "estado" : "",
+        "cidade" : "",
+        "bairro" : "",
+        "endereco" : "",
+        "numero" : "",
+        "complemento" : "",
+        "dataNascimento" : "",
+        "universidade" : "",
+        "curso" : "",
+        "periodo" : "",
+        "ej" : "",
+        "cargoEj" : "",
+        "tamanhoCamisa" : "",
+        "necessidades" : "",
+        "descNecessidade" : "",
+        "dieta" : "",
+        "alergia" : "",
+    };
+});
